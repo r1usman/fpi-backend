4e73f6bd3b2f8f3efac9b30c1c2f3a38dc5a8aac
Usman Akram <ru0300usman@gmail.com>
2025-05-21 14:38:31 +0500

    1st backend commit

---
+++ api/index.js
@@ -0,0 +1,44 @@
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import path from "path";
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const __dirname = path.resolve();
+
+const app = express();
+
+// for s3 use
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
