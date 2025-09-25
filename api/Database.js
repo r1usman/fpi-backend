6f050f8e51ac7c6d7a6c5d3e0260424eaad8ef12
usman <ru0300usman@gmail.com>
2025-09-25 17:00:33 +0500

    db resumed

--- api/Database.js
+++ api/Database.js
@@ -1,14 +1,12 @@
 import mongoose from "mongoose";
 
 const ConnectDb = async (MongoURL) => {
-    try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/FYP")
-        console.log("MongoDB is Connected");
+  try {
+    await mongoose.connect(MongoURL);
+    console.log("MongoDB is Connected");
+  } catch (error) {
+    console.log("error", error);
+  }
+};
 
-
-    } catch (error) {
-        console.log("error", error);
-    }
-}
-
-export default ConnectDb
+export default ConnectDb;
