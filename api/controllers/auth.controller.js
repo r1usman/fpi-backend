03f5449c2e4a6714a79399b704c4c7ee509f521e
usman <ru0300usman@gmail.com>
2025-09-28 20:38:07 +0500

    backend usman updated

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -13,7 +13,7 @@ const google = async (req, res, next) => {
       res.cookie("FreashToken", token, {
         httpOnly: true,
         secure: false,
-        sameSite: "Lax",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
       });
       return res.status(200).json({
