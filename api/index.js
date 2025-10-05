b876f87f3b4852953dc13f96ab364edcf395eaab
obaiddp <obaidullahzeb38@gmail.com>
2025-10-05 17:15:44 +0500

    Badge related fix

--- api/index.js
+++ api/index.js
@@ -19,8 +19,8 @@ const AskAi = require("./routes/AI_Routes.js")
 
 const SingleProblemRoutes = require("./routes/singleProblemRoute.js");
 const SingleSubmissionRoutes = require("./routes/singleSubmissionRoute.js");
-const badgeRoutes = require("./routes/badgeRoutes");
-const certificationRoutes = require("./routes/certificationRoutes");
+// const badgeRoutes = require("./routes/badgeRoutes");
+// const certificationRoutes = require("./routes/certificationRoutes");
 
 dotenv.config();
 
@@ -84,8 +84,8 @@ app.use("/Ask", AskAi);
 // Problem Routes
 app.use("/api/problems", SingleProblemRoutes);
 app.use("/api/submissions", SingleSubmissionRoutes);
-app.use("/api/badges", badgeRoutes);
-app.use("/api/certifications", certificationRoutes);
+// app.use("/api/badges", badgeRoutes);
+// app.use("/api/certifications", certificationRoutes);
 
 app.get("/", async (req, res) => {
   try {
