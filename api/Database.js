b3b066f4c8a40bdb4769de65dd9ba5061452cc4c
usman <ru0300usman@gmail.com>
2025-10-07 21:34:55 +0500

    last commit

--- api/Database.js
+++ api/Database.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const ConnectDb = async (MongoURL) => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/FYP");
+    // await mongoose.connect(MongoURL);
     console.log("MongoDB is Connected");
   } catch (error) {
     console.log("error", error);
