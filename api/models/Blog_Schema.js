bad5a54f25b34ac2d853e2cff13c17ffb279cc45
Ali-dotcom98 <alishah1234584.as@gmail.com>
2025-11-13 21:30:31 +0500

    Update

---
+++ api/models/Blog_Schema.js
@@ -0,0 +1,23 @@
+const { Mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+
+const BlogPostSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        slug: { type: String, required: true, unique: true },
+        content: { type: String, required: true }, // markdown content
+        coverImageUrl: { type: String, default: null },
+        tags: [{ type: String }],
+        isDraft: { type: Boolean, default: false },
+        views: { type: Number, default: 0 },
+        likes: { type: Number, default: 0 },
+        generatedByAI: { type: Boolean, default: false },
+        BelongTo: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "CoursesCategory",
+        }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("BlogPost", BlogPostSchema);
